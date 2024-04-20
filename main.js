623ed0a745f8b9ddaa4dac49990b547029d857e7
Abrar2165 <108695905+Abrar2165@users.noreply.github.com>
2024-04-20 12:48:28 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,23 @@
-function setup()
-{
-    canvas = createCanvas(500, 500);
+song = "";
+
+function setup() {
+    canvas = createCanvas(600, 500);
     canvas.center();
+
+    video = createCapture(VIDEO);
+    video.hide();
+}
+
+function draw() {
+    image(video, 0, 0, 600, 500,);
+}
+
+function preload()
+{
+    song = loadSound("music.mp3");
+}
+
+function play()
+{
+    song.play();
 }
